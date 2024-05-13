c692918de1219f43e67c8eeb5b9dfae4a48daa7d
Nguyen-Thac-Bach <129993123+Nguyen-Thac-Bach@users.noreply.github.com>
2024-05-14 00:38:08 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="RAIT",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Nguyen Thac Bach",
+    description="Python implementation of RAIT from matlab",
+)
