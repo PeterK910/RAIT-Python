76795cefdf8b6f20772332dc67b32ee034cfed27
Nguyen-Thac-Bach <129993123+Nguyen-Thac-Bach@users.noreply.github.com>
2024-07-27 15:16:19 +0200

    test arg_der

--- tests/test_blaschke.py
+++ tests/test_blaschke.py
@@ -209,3 +209,67 @@ def test_argdr_inv():
         argdr_inv(a, b)
     except ValueError:
         pytest.fail('argdr_inv raised ValueError unexpectedly!')
+
+def test_arg_der():
+    from .blaschke import arg_der
+    a = torch.tensor([-0.5j,0,0.5], dtype=torch.complex64)
+    t = torch.tensor([0.1, 0.2, 0.3], dtype=torch.float64)
+    expected_result = torch.tensor([1.498950, 1.432060, 1.343517], dtype=torch.float64)
+    assert torch.allclose(arg_der(a, t), expected_result)
+
+    #input validation
+    #a
+    regex = re.compile(re.escape('"a" must be a torch.Tensor.'))
+    with pytest.raises(TypeError, match=regex):
+        a = [1, 2, 3]
+        t = torch.tensor([1.0, 2.0, 3.0], dtype=torch.float64)
+        arg_der(a, t)
+
+    regex = re.compile(re.escape('"a" must be a complex torch.Tensor.'))
+    with pytest.raises(TypeError, match=regex):
+        a = torch.tensor([1.0, 2.0, 3.0])
+        t = torch.tensor([1.0, 2.0, 3.0], dtype=torch.float64)
+        arg_der(a, t)
+
+    regex = re.compile(re.escape('"a" must be a 1-dimensional torch.Tensor.'))
+    with pytest.raises(ValueError, match=regex):
+        a = torch.tensor([[0.5, 0.5, 0.5], [0.5, 0.5, 0.5]], dtype=torch.complex64)
+        t = torch.tensor([1.0, 2.0, 3.0], dtype=torch.float64)
+        arg_der(a, t)
+
+    regex = re.compile(re.escape('Elements of "a" must be inside the unit circle!'))
+    with pytest.raises(ValueError, match=regex):
+        a = torch.tensor([0.5, 0.5, 1], dtype=torch.complex64)
+        t = torch.tensor([1.0, 2.0, 3.0], dtype=torch.float64)
+        arg_der(a, t)
+
+    #t
+    regex = re.compile(re.escape('"t" must be a torch.Tensor.'))
+    with pytest.raises(TypeError, match=regex):
+        a = torch.tensor([0.5, 0.5, 0.5], dtype=torch.complex64)
+        t = [1, 2, 3]
+        arg_der(a, t)
+
+    regex = re.compile(re.escape('"t" must be a torch.Tensor with float64 dtype.'))
+    with pytest.raises(TypeError, match=regex):
+        a = torch.tensor([0.5, 0.5, 0.5], dtype=torch.complex64)
+        t = torch.tensor([1.0, 2.0, 3.0], dtype=torch.float32)
+        arg_der(a, t)
+
+    regex = re.compile(re.escape('"t" must be a 1-dimensional torch.Tensor.'))
+    with pytest.raises(ValueError, match=regex):
+        a = torch.tensor([0.5, 0.5, 0.5], dtype=torch.complex64)
+        t = torch.tensor([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]], dtype=torch.float64)
+        arg_der(a, t)
+
+    regex = re.compile(re.escape('Elements of "t" must be in [-pi, pi).'))
+    with pytest.raises(ValueError, match=regex):
+        a = torch.tensor([0.5, 0.5, 0.5], dtype=torch.complex64)
+        t = torch.tensor([1.0, 2.0, torch.pi], dtype=torch.float64)
+        arg_der(a, t)
+    try:
+        a = torch.tensor([0, 0, 0], dtype=torch.complex64)
+        t = torch.tensor([-torch.pi, 2.0, 3.0], dtype=torch.float64) # -pi is valid
+        arg_der(a, t)
+    except ValueError:
+        pytest.fail('argdr_fun raised ValueError unexpectedly!')
