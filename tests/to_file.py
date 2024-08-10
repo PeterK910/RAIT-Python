54d6ba9588a00263d3c8fe2b822f9793ae656ffd
Nguyen-Thac-Bach <129993123+Nguyen-Thac-Bach@users.noreply.github.com>
2024-08-10 18:51:15 +0200

    attempt writing output to file

---
+++ tests/to_file.py
@@ -0,0 +1,13 @@
+
+import biort_sys
+import blaschke
+import mt_sys
+import rat_sys
+import simplex
+import util
+
+def output_to_file(function_name, arg1=0, arg2=0, arg3=0):
+    #print(f"{globals()[function_name].__doc__}")
+    print(f"{globals()}")
+    """ for i in range(5):
+        print(f"globals()[{i}]: {globals()[i]}") """
