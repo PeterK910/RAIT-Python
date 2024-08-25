9885295e248fb55766f5773d1c3f79422a6ce022
Nguyen-Thac-Bach <129993123+Nguyen-Thac-Bach@users.noreply.github.com>
2024-08-25 23:34:04 +0200

    add misc files
    like readme update, setup.py, setup.cfg

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-"# RAIT-Python" 
+# RAIT-Python
+## Python implementation of RAIT library
+
+Requires Pytorch >= 2.3 (likely runs on lower versions, though untested)
+
+## Presentation
+
+This repository implements the RAIT matlab library
+Currently the following functions are not implemented:
+
+Functions that plot:
+arg_inv_anim
+blaschkes_img
+periodize
+rshow
+Functions starting with simplex:
+simplex_biort
+simplex_biortdc
+simplex_mt
+simplex_mtdc
+simplex_mtdr
+
+License is also missing
+
+## Installation
+
+Type `pip install -e .` in the root folder of this repo.
+
+## Testing
+
+pip install pytest
+python -m pytest
+
+
+
+
--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
 from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
-    name="RAIT",
-    version="0.1.0",
-    packages=find_packages(),
-    install_requires=[],
-    author="Nguyen Thac Bach",
+    name="rait",
+    version="0.9.0",
+
     description="Python implementation of RAIT from matlab",
+    url="https://github.com/Nguyen-Thac-Bach/RAIT-Python",
+    
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[
+        'torch>=2.3',
+    ],
+    author="Thac Bach Nguyen",
 )
